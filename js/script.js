830f06a303f914bb34ae8b8453d7f81f17aa7f80
kevinnarbas <kevinnarbas@git.generalassemb.ly>
2019-08-04 07:27:56 -0500

    builds master deck

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 /*----- constants -----*/ 
 
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14'];
+
+const masterDeck = builtMasterDeck();
 
 /*----- app's state (variables) -----*/ 
-let cardCount, currCard, playDeck, winner;
+let cardCount, currCard, playDeck, compDeck, winner;
 
 /*----- cached element references -----*/ 
 
@@ -14,6 +18,33 @@ let cardCount, currCard, playDeck, winner;
 init();
 
 function init() {
-    cardCount = 0;
-    
+    cardCount = {
+        player: 0,
+        comp: 0,
+    };
+
+    currCard = {
+        player: 'back',
+        comp: 'back',
+    };
+
+    playDeck = [];
+
+    compDeck = [];
+
+    winner = null // player or comp 
 }
+
+function builtMasterDeck() {
+    let deck = [];
+    suits.forEach(function(suit) {
+        ranks.forEach(function (rank) {
+            deck.push({
+                face: `${suit}${rank}`,
+                value: Number(rank),
+                
+            });
+        });
+    });
+    console.log(deck)
+};
